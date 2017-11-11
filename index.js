2c08ac5ae59ef1a01d2b6dae702c5e506aba4f2e
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-11 07:25:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file
+function findMatching(drivers,name) {
+  let results  = []
+  for (const element of drivers) {
+    if (element.toUpperCase() == name.toUpperCase()) {
+    results.push(element)
+    }
+  }
+  return results
+}
+
+function fuzzyMatch(drivers,letters) {
+  return drivers.filter(function(el) {
+    return el.toLowerCase().indexOf(letters.toLowerCase()) > -1 &&
+      el.toLowerCase().indexOf(letters.toLowerCase()) < letters.length;
+  })
+}
+
+function matchName (collection, searcher) {
+  const newCollection = [];
+
+  for (const user of collection) {
+    if (user.name == searcher) {
+      newCollection.push(user);
+    }
+  }
+
+  return newCollection;
+}
